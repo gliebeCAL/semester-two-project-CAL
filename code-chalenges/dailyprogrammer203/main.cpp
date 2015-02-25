36a32e9ab7d3cf24b945135a539fe0965129f20a
Gliebe <gliebereader@gmail.com>
2015-02-25 10:39:11 -0800

    Updated documentation for Monday and Wednesday; started new chalenge

---
+++ code-chalenges/dailyprogrammer203/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	int nWidth;
+	int nArea;
+
+	cout << "I heard you wanted to make a square.  Well?  Tell me stuff already!\n";
+	cout << "How many units wide do you want it: ";
+	cin >> nWidth;
+
+	int nArea = nWidth * nWidth;
+
+	for (int iii = 1; iii <= nArea; iii++)
+	{
+		if (iii nWidth)
+		cout << "=";
+	}
+
+}
