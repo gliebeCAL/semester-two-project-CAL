18543a75ebfb80be776c266140ac40c0df45ddd7
Gliebe <gliebereader@gmail.com>
2015-03-02 22:00:35 -0800

    Completed dailyprogrammer203 challenge

--- code-challenges/dailyprogrammer203/main.cpp
+++ code-challenges/dailyprogrammer203/main.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 
+void squarePrint(int nWidth)
+{
+	// This function's purpose is to print a square based on user input
+
+	using namespace std;
+
+	int nArea = nWidth * nWidth;
+
+	for (int iii = 1, currentWidth = 1; iii <= nArea; iii++)
+	{
+		if (currentWidth < nWidth)
+		{
+			cout << "+";
+			currentWidth++;
+		}
+		else if (currentWidth == nWidth)
+		{
+			cout << "+" << endl;
+			currentWidth = 1;
+		}
+	}
+}
+
 int main()
 {
+	using namespace std;
+
 	int nWidth;
-	int nArea;
 
 	cout << "I heard you wanted to make a square.  Well?  Tell me stuff already!\n";
 	cout << "How many units wide do you want it: ";
 	cin >> nWidth;
+	// Todo: Write a check to ensure value is the correct type
 
-	int nArea = nWidth * nWidth;
+	squarePrint(nWidth);
 
-	for (int iii = 1; iii <= nArea; iii++)
-	{
-		if (iii nWidth)
-		cout << "=";
-	}
-
+	return 0;
 }
